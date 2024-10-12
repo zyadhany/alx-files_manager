66bbe2a80986c8567fa2055509a246b8e1551caf
Zyad Hany <102915260+zyadhany@users.noreply.github.com>
2024-10-12 22:45:19 +0000

    task #1

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import AppController from '../controllers/AppController';
+
+const router = express.Router();
+
+router.get('/status', (req, res) => {
+    AppController.getStatus(req, res);
+});
+
+router.get('/stats', (req, res) => {
+    AppController.getStats(req, res);  
+});
+
+export default router;
---
+++ server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import router from './routes/index.js';
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.use(express.json());
+app.use('/', router);
+
+app.listen(PORT, () => {
+  console.log(`Server running http://localhost:${PORT}`);
+});
+
+export default app;
