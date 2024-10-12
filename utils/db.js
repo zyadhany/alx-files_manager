3c926ec7ad4d09edd64ad2470dd751106c74eb45
Zyad Hany <102915260+zyadhany@users.noreply.github.com>
2024-10-12 21:43:27 +0000

    task #1

---
+++ utils/db.js
@@ -0,0 +1,38 @@
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+
+    this.client = new MongoClient(`mongodb://${host}:${port}`, { useUnifiedTopology: true });
+    this.client.connect();
+    this.db = this.client.db(database);
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    return new Promise((resolve, reject) => {
+      this.db.collection('users').find().count((err, count) => {
+        if (err) reject(err);
+        resolve(count);
+      });
+    });
+  }
+
+  async nbFiles() {
+    return new Promise((resolve, reject) => {
+      this.db.collection('files').find().count((err, count) => {
+        if (err) reject(err);
+        resolve(count);
+      });
+    });
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
