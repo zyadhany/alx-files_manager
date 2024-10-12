e47ea11ea8b7da88ec8c98f2bbd293129b8ed457
Zyad Hany <102915260+zyadhany@users.noreply.github.com>
2024-10-12 22:41:41 +0000

    task #1

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -5,6 +5,12 @@ class AppController {
   static getStatus(req, res) {
     res.status(200).send({ redis: redisClien.isAlive(), db: dbClient.isAlive() });
   }
+
+  static async getStats(req, res) {
+    const users = await dbClient.nbUsers();
+    const files = await dbClient.nbFiles();
+    res.status(200).send({ users, files });
+  }
 }
 
 export default AppController;
