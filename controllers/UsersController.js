e9403dfb0b75bcf6f28fcd83ece8cc6cce66de39
Zyad Hany <102915260+zyadhany@users.noreply.github.com>
2024-10-15 21:38:14 +0000

    file

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,29 +1,29 @@
 import sha1 from 'sha1';
 import dbClient from '../utils/db';
 
-class UsersController {
+export default class UsersController {
   static async postNew(req, res) {
     const email = req.body ? req.body.email : null;
     const password = req.body ? req.body.password : null;
 
     if (!email) {
-      return res.status(400).json({ error: 'Missing email' });
+      res.status(400).json({ error: 'Missing email' });
+      return;
     }
     if (!password) {
-      return res.status(400).json({ error: 'Missing password' });
+      res.status(400).json({ error: 'Missing password' });
+      return;
     }
-
-    const hpassword = sha1(password);
-
     const user = await (await dbClient.usersCollection()).findOne({ email });
+
     if (user) {
       res.status(400).json({ error: 'Already exist' });
-      return
+      return;
     }
     const insertionInfo = await (await dbClient.usersCollection())
-      .insertOne({ email, password: hpassword });
-    return res.status(201).json({ email, id: insertionInfo.insertedId });
+      .insertOne({ email, password: sha1(password) });
+    const userId = insertionInfo.insertedId.toString();
+
+    res.status(201).json({ email, id: userId });
   }
 }
-
-export default UsersController;
